361f6f0319c0598e1f4751827f4dfb0ac3285fc2
goldenratio <karthikeyanvj@gmail.com>
2025-12-01 22:27:54 +0200

    added math utils

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,5 @@ export * from "./types.js";
 export * from "./dispose_bag.js";
 export * from "./pool_bag.js";
 export * from "./assert_utils.js";
+export * from "./math_utils.js";
 export { NOOP } from "./noop.js";
