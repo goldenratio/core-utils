f850619619d88235b35f164c7fd988b31fa10f52
goldenratio <karthikeyanvj@gmail.com>
2025-12-01 22:44:41 +0200

    added type util

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,5 @@ export * from "./dispose_bag.js";
 export * from "./pool_bag.js";
 export * from "./assert_utils.js";
 export * from "./math_utils.js";
+export * from "./type_utils.js";
 export { NOOP } from "./noop.js";
