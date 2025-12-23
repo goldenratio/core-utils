2fec75092ca88063847931659ed4bb488747e83d
goldenratio <karthikeyanvj@gmail.com>
2025-12-23 22:58:50 +0200

    feat: added abort signal to debounce fn

--- src/debounce.ts
+++ src/debounce.ts
@@ -3,15 +3,40 @@
  */
 export function debounce<T extends (...args: Parameters<T>) => void>(
   fn: T,
-  delay: number = 100
+  delay: number,
+  options?: { readonly signal?: AbortSignal },
 ): (...args: Parameters<T>) => void {
   let timer: number | undefined = undefined;
 
-  return function(this: ThisParameterType<T>, ...args: Parameters<T>) {
+  const on_abort = (): void => {
     if (timer !== undefined) {
-      clearTimeout(timer);
+      window.clearTimeout(timer);
     }
+    timer = undefined;
+  };
+
+  if (options?.signal) {
+    if (options.signal.aborted) {
+      on_abort();
+    } else {
+      options.signal.addEventListener("abort", on_abort, { once: true });
+    }
+  }
+
+  return function(this: ThisParameterType<T>, ...args: Parameters<T>) {
+    if (options?.signal?.aborted) {
+      return;
+    }
+
+    if (timer !== undefined) {
+      window.clearTimeout(timer);
+    }
+
     timer = window.setTimeout(() => {
+      timer = undefined;
+      if (options?.signal?.aborted) {
+        return;
+      }
       fn.apply(this, args);
     }, delay);
   };
